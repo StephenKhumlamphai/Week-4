0bd6eea62bebb9291372c36dc940f068214a26cc
StephenKhumlamphai <stephen.khumlamphai-1@student.uts.edu.au>
2022-08-28 21:19:34 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,16 +6,23 @@ public class PrintAndHide : MonoBehaviour
 {
 
     public int i = 3;
+    public int randInt;
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        randInt = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ":" + ++i);
+        if (gameObject.tag == "Red" && i == 100) {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == randInt) {
+            rend.enabled = !rend.enabled;
+        }
     }
 }
